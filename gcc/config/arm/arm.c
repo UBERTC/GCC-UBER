1f5ca39a0487a195c52793a8a09e9803f50a8a99
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-05 12:38:03 +0000

    gcc/ 2013-09-05  Christophe Lyon  <christophe.lyon@linaro.org>
    	Backport from trunk r199527,199792,199814.
    	2013-05-31  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/56315
    	* config/arm/arm.c (const_ok_for_dimode_op): Handle IOR.
    	* config/arm/arm.md (*iordi3_insn): Change to insn_and_split.
    	* config/arm/neon.md (iordi3_neon): Remove.
    	(neon_vorr<mode>): Generate iordi3 instead of iordi3_neon.
    	* config/arm/predicates.md (imm_for_neon_logic_operand):
    	Move to earlier in the file.
    	(neon_logic_op2): Likewise.
    	(arm_iordi_operand_neon): New predicate.
    
    	2013-06-07  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	* config/arm/constraints.md (Df): New constraint.
    	* config/arm/arm.md (iordi3_insn): Use Df constraint instead of De.
    	Correct length attribute for last two alternatives.
    
    	2013-06-07  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/56315
    	* config/arm/arm.md (*xordi3_insn): Change to insn_and_split.
    	(xordi3): Change operand 2 constraint to arm_xordi_operand.
    	* config/arm/arm.c (const_ok_for_dimode_op): Handle XOR.
    	* config/arm/constraints.md (Dg): New constraint.
    	* config/arm/neon.md (xordi3_neon): Remove.
    	(neon_veor<mode>): Generate xordi3 instead of xordi3_neon.
    	* config/arm/predicates.md (arm_xordi_operand): New predicate.
    
    gcc/testsuite/
    2013-09-05  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	Backport from trunk r199527,199814,201435.
    	2013-05-31  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/56315
    	* gcc.target/arm/iordi3-opt.c: New test.
    
    	2013-06-07  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/56315
    	* gcc.target/arm/xordi3-opt.c: New test.
    
    	2013-08-02  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	* gcc.target/arm/neon-for-64bits-2.c: Delete.
    
    
    
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_8-branch@202280 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -2685,6 +2685,8 @@ const_ok_for_dimode_op (HOST_WIDE_INT i, enum rtx_code code)
   switch (code)
     {
     case AND:
+    case IOR:
+    case XOR:
       return (const_ok_for_op (hi_val, code) || hi_val == 0xFFFFFFFF)
               && (const_ok_for_op (lo_val, code) || lo_val == 0xFFFFFFFF);
     case PLUS:
