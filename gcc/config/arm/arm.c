dd2f3dff60b6d95a003a47f9626f4f48d5186115
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-08 14:22:10 +0000

    gcc/ 2013-11-05  Zhenqiang Chen <zhenqiang.chen@linaro.org>
    	Backport from trunk r203267, r203603 and r204247.
    	2013-10-08  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	PR target/58423
    	* config/arm/arm.c (arm_emit_ldrd_pop): Attach
    	RTX_FRAME_RELATED_P on INSN.
    
    	2013-10-15  Matthew Gretton-Dann  <matthew.gretton-dann@arm.com>
    	    Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	* config/arm/t-aprofile: New file.
    	* config.gcc: Handle --with-multilib-list option.
    
    	2013-10-31  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	* lower-subreg.c (resolve_simple_move): Copy REG_INC note.
    
    gcc/testsuite/
    2013-11-05  Zhenqiang Chen <zhenqiang.chen@linaro.org>
    
     	Backport from trunk r204247.
     	2013-10-31  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
     	 * gcc.target/arm/lp1243022.c: New test.
    
    
    
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_8-branch@204570 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -17607,8 +17607,8 @@ arm_emit_ldrd_pop (unsigned long saved_regs_mask)
               mem = gen_frame_mem (DImode, stack_pointer_rtx);
 
             tmp = gen_rtx_SET (DImode, gen_rtx_REG (DImode, j), mem);
-            RTX_FRAME_RELATED_P (tmp) = 1;
             tmp = emit_insn (tmp);
+	    RTX_FRAME_RELATED_P (tmp) = 1;
 
             /* Generate dwarf info.  */
 
@@ -17636,8 +17636,8 @@ arm_emit_ldrd_pop (unsigned long saved_regs_mask)
               mem = gen_frame_mem (SImode, stack_pointer_rtx);
 
             tmp = gen_rtx_SET (SImode, gen_rtx_REG (SImode, j), mem);
-            RTX_FRAME_RELATED_P (tmp) = 1;
             tmp = emit_insn (tmp);
+	    RTX_FRAME_RELATED_P (tmp) = 1;
 
             /* Generate dwarf info.  */
             REG_NOTES (tmp) = alloc_reg_note (REG_CFA_RESTORE,
@@ -17661,8 +17661,9 @@ arm_emit_ldrd_pop (unsigned long saved_regs_mask)
                          plus_constant (Pmode,
                                         stack_pointer_rtx,
                                         offset));
-      RTX_FRAME_RELATED_P (tmp) = 1;
-      emit_insn (tmp);
+      tmp = emit_insn (tmp);
+      arm_add_cfa_adjust_cfa_note (tmp, offset,
+				   stack_pointer_rtx, stack_pointer_rtx);
       offset = 0;
     }
 
@@ -17685,6 +17686,8 @@ arm_emit_ldrd_pop (unsigned long saved_regs_mask)
                               gen_rtx_REG (SImode, PC_REGNUM),
                               NULL_RTX);
       REG_NOTES (par) = dwarf;
+      arm_add_cfa_adjust_cfa_note (par, UNITS_PER_WORD,
+				   stack_pointer_rtx, stack_pointer_rtx);
     }
 }
 
