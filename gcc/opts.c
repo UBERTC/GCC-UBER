7ab1703a76d08fa29fc6c603ed55369cf67ec494
Chet Kener <Cl3Kener@gmail.com>
2014-04-26 19:53:42 -0400

    Link LD to Match Google 4.7
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- gcc/opts.c
+++ gcc/opts.c
@@ -1735,6 +1735,7 @@ common_handle_option (struct gcc_options *opts,
       dc->max_errors = value;
       break;
 
+    case OPT_fuse_ld_:
     case OPT_fuse_linker_plugin:
       /* No-op. Used by the driver and passed to us because it starts with f.*/
       break;
