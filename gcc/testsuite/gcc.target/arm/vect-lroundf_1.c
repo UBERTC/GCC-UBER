33f5dab31de4bec5c681b62a11a69eef07516db9
yroux <yroux@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-08 15:37:43 +0000

    gcc/ 2014-10-08  Yvan Roux  <yvan.roux@linaro.org>
    	Backport from trunk r214825, r214826.
    	2014-09-02  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/62275
    	* config/arm/neon.md
    	(neon_vcvt<NEON_VCVT:nvrint_variant><su_optab><VCVTF:mode>
    	<v_cmp_result>): New pattern.
    	* config/arm/iterators.md (NEON_VCVT): New int iterator.
    	* config/arm/arm_neon_builtins.def (vcvtav2sf, vcvtav4sf, vcvtauv2sf,
    	vcvtauv4sf, vcvtpv2sf, vcvtpv4sf, vcvtpuv2sf, vcvtpuv4sf, vcvtmv2sf,
    	vcvtmv4sf, vcvtmuv2sf, vcvtmuv4sf): New builtin definitions.
    	* config/arm/arm.c (arm_builtin_vectorized_function): Handle
    	BUILT_IN_LROUNDF, BUILT_IN_LFLOORF, BUILT_IN_LCEILF.
    
    	2014-09-02  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/62275
    	* config/arm/iterators.md (FIXUORS): New code iterator.
    	(VCVT): New int iterator.
    	(su_optab): New code attribute.
    	(su): Likewise.
    	* config/arm/vfp.md (l<vrint_pattern><su_optab><mode>si2): New pattern.
    
    gcc/testsuite/
    2014-10-08  Yvan Roux  <yvan.roux@linaro.org>
    
    	Backport from trunk r214825, r214826, r215085.
    	2014-09-09  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	* gcc.target/arm/vect-lceilf_1.c: Make input and output arrays global
    	and 16-byte aligned.
    	* gcc.target/arm/vect-lfloorf_1.c: Likewise.
    	* gcc.target/arm/vect-lroundf_1.c: Likewise.
    	* gcc.target/arm/vect-rounding-btruncf.c: Likewise.
    	* gcc.target/arm/vect-rounding-ceilf.c: Likewise.
    	* gcc.target/arm/vect-rounding-floorf.c: Likewise.
    	* gcc.target/arm/vect-rounding-roundf.c: Likewise.
    
    	2014-09-02  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/62275
    	* gcc.target/arm/vect-lceilf_1.c: New test.
    	* gcc.target/arm/vect-lfloorf_1.c: Likewise.
    	* gcc.target/arm/vect-lroundf_1.c: Likewise.
    
    	2014-09-02  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/62275
    	* gcc.target/arm/lceil-vcvt_1.c: New test.
    	* gcc.target/arm/lfloor-vcvt_1.c: Likewise.
    	* gcc.target/arm/lround-vcvt_1.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_9-branch@216007 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/vect-lroundf_1.c
+++ gcc/testsuite/gcc.target/arm/vect-lroundf_1.c
@@ -1,12 +1,12 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target arm_v8_neon_ok } */
-/* { dg-options "-O2 -ffast-math -ftree-vectorize" } */
+/* { dg-options "-O2 -ffast-math -ftree-vectorize -fdump-tree-vect-all" } */
 /* { dg-add-options arm_v8_neon } */
 
 #define N 32
 
 float __attribute__((aligned(16))) input[N];
-float __attribute__((aligned(16))) output[N];
+int __attribute__((aligned(16))) output[N];
 
 void
 foo ()
@@ -14,8 +14,8 @@ foo ()
   int i = 0;
   /* Vectorizable.  */
   for (i = 0; i < N; i++)
-    output[i] = __builtin_truncf (input[i]);
+    output[i] = __builtin_lroundf (input[i]);
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target vect_call_btruncf } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
