b7b068b2c39f8a9a39e15eba859665aaf382061e
yroux <yroux@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-09 01:11:37 +0000

    2015-02-09  Yvan Roux  <yvan.roux@linaro.org>
    	Backport from trunk r217173, r217174, r217687.
    	2014-11-17  Terry Guo  <terry.guo@arm.com>
    
    	* config/arm/arm.c (arm_issue_rate): Return 2 for cortex-m7.
    	* config/arm/arm.md (generic_sched): Exclude cortex-m7.
    	(generic_vfp): Likewise.
    	* config/arm/cortex-m7.md: Pipeline description for cortex-m7.
    
    	2014-10-06  Hale Wang  <Hale.Wang@arm.com>
    
    	* config/arm/arm.c: Add cortex-m7 tune.
    	* config/arm/arm-cores.def: Use cortex-m7 tune.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_9-branch@220527 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -1970,6 +1970,25 @@ const struct tune_params arm_v7m_tune =
   false, false                                  /* Prefer 32-bit encodings.  */
 };
 
+/* Cortex-M7 tuning.  */
+
+const struct tune_params arm_cortex_m7_tune =
+{
+  arm_9e_rtx_costs,
+  &v7m_extra_costs,
+  NULL,						/* Sched adj cost.  */
+  0,						/* Constant limit.  */
+  0,						/* Max cond insns.  */
+  ARM_PREFETCH_NOT_BENEFICIAL,
+  true,						/* Prefer constant pool.  */
+  arm_cortex_m_branch_cost,
+  false,					/* Prefer LDRD/STRD.  */
+  {true, true},					/* Prefer non short circuit.  */
+  &arm_default_vec_cost,                        /* Vectorizer costs.  */
+  false,                                        /* Prefer Neon for 64-bits bitops.  */
+  false, false                                 /* Prefer 32-bit encodings.  */
+};
+
 /* The arm_v6m_tune is duplicated from arm_cortex_tune, rather than
    arm_v6t2_tune. It is used for cortex-m0, cortex-m1 and cortex-m0plus.  */
 const struct tune_params arm_v6m_tune =
@@ -29813,6 +29832,7 @@ arm_issue_rate (void)
     case cortexa57:
       return 3;
 
+    case cortexm7:
     case cortexr4:
     case cortexr4f:
     case cortexr5:
